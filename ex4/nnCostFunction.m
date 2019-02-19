15ffc4e67f85a9914911bb11fa53ced566dce56a
nordinh <nordin.haouari@gmail.com>
2019-02-19 23:51:05 +0100

    implemented regularized gradient

--- ex4/nnCostFunction.m
+++ ex4/nnCostFunction.m
@@ -89,8 +89,8 @@ endfor
 Theta2_grad = 1/m * Theta2_grad;
 Theta1_grad = 1/m * Theta1_grad;
 
-
-
+Theta2_grad(:,2:end) = Theta2_grad(:,2:end) + (lambda/m)*Theta2(:,2:end);
+Theta1_grad(:,2:end) = Theta1_grad(:,2:end) + (lambda/m)*Theta1(:,2:end);
 
 
 
