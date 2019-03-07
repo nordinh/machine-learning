df32bec481635104b5ff6bf72e6eb7dd00ceaab8
nordinh <nordin.haouari@gmail.com>
2019-03-07 07:57:25 +0100

    finished first part of ex6

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -23,11 +23,29 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+Copt = [0.01;0.03;0.1;0.3;1;3;10;30];
+sigmaopt = [0.01;0.03;0.1;0.3;1;3;10;30];
 
+optionResults = zeros(64, 3);
 
+resultIndex = 0;
+for i=1:size(Copt)
+  for j=1:size(sigmaopt)
+    resultIndex = resultIndex + 1;
+    
+    model = svmTrain(X, y, Copt(i), @(x1, x2) gaussianKernel(x1, x2, sigmaopt(j)));
+    predictions = svmPredict(model, Xval);
+    
+    optionResults(resultIndex, 1) = Copt(i);
+    optionResults(resultIndex, 2) = sigmaopt(j);
+    optionResults(resultIndex, 3) = mean(double(predictions ~= yval));
+  endfor
+endfor
 
+[minError minIndex] = min(optionResults(:,3));
 
-
+C = optionResults(minIndex, 1);
+sigma = optionResults(minIndex, 2);
 
 % =========================================================================
 
