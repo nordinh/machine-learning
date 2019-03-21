78912b90672e1e630598c04e92abd6d337a30114
nordinh <nordin.haouari@gmail.com>
2019-03-21 22:38:50 +0100

    finished cost function movie recommender system

--- ex8/cofiCostFunc.m
+++ ex8/cofiCostFunc.m
@@ -40,11 +40,9 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
-
-
-
-J = sum(sum(R.*((X*Theta' - Y).^2)))/2;
-
+J = sum(sum(R.*((X*Theta' - Y).^2)))/2 + (lambda/2 * sum(sum(Theta.^2))) + (lambda/2 * sum(sum(X.^2)));
+X_grad = R.*(X*Theta' - Y)*Theta + lambda*X;
+Theta_grad = (R.*(X*Theta' - Y))'*X + lambda*Theta;
 
 
 
