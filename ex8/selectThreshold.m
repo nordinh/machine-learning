11622ba35a6684cabc52210ab120457617bcd930
nordinh <nordin.haouari@gmail.com>
2019-03-21 07:21:11 +0100

    finished anomaly detection

--- ex8/selectThreshold.m
+++ ex8/selectThreshold.m
@@ -23,17 +23,18 @@ for epsilon = min(pval):stepsize:max(pval)
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
 
-
-
-
-
-
-
-
-
-
-
-
+    tp = sum((pval < epsilon) & (yval == 1));
+    fp = sum((pval < epsilon) & (yval == 0));
+    fn = sum((pval >= epsilon) & (yval == 1));
+    if tp == 0
+      prec = 0;
+      rec = 0;
+      F1 = 0;
+    else
+      prec = tp / (tp + fp);
+      rec = tp / (tp + fn);
+      F1 = (2*prec*rec)/(prec+rec);
+    end
 
     % =============================================================
 
